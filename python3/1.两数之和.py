b727161e8ade9f24f02f0f0101d5374286f0adb3
Lynch <lqhan@bigquant.ai>
2023-02-02 23:25:02 +0800

    ✨ feat(main): 2023-02-02 1.两数之和 python3

---
+++ python3/1.两数之和.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 1. 暴力法
+        # for i in range(len(nums) - 1):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # 2. 两遍哈希表
+        r: Dict[int, int] = {}
+        for i, v in enumerate(nums):
+            if target - v in r:
+                return [r[target - v], i]
+            r[v] = i
+        print(r)
+        return []
+
+
+# @lc code=end
